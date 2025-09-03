de2cb36c43beab447216cf699ca95b598b83d659
Emerson do carmo cristo <Emersoncristo77@icloud.com>
2025-09-03 11:44:24 -0300

    01

--- notaalu.py
+++ notaalu.py
@@ -1,8 +1,8 @@
 nota = int(input("Digite sua nota:"))
 
-if nota >= 8:
+if nota >= 8:  #maior igual a 8
     print("Ótima nota!")
-elif nota >= 6:
+elif nota >= 6: #maior igual a 6
     print("Melhore sua nota!")
-else:
+else: # se não
     print("Recuperação")
