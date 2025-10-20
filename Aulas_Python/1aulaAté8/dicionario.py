601b35a216e90e8553da4d7ad97ea0521ef56077
Emerson do carmo cristo <Emersoncristo77@icloud.com>
2025-10-20 20:59:42 -0300

    att20/10

--- Aulas_Python/1aulaAté8/dicionario.py
+++ Aulas_Python/1aulaAté8/dicionario.py
@@ -27,3 +27,20 @@ print(aluno)
 
 del aluno["Curso"]
 print(aluno)
+
+# quando vai utilizar o for em um dicionario é necessário utilizar o .item no final, para o mesmo trazer os dados do dicionario para a tela 
+
+dados = {"Nome":"Lan Code", "Inscritos":5000, "Categoria":"Programação"}
+
+for c, v in dados.items():
+    print(f"{c}: {v}")
+
+#quando se vai utilizar o for em dicionario para buscar apenas 1 dos 2 items, chave ou valores a semantica muda 
+
+#quando se vai fazer com os valores se faz assim 
+for v, in dados.values():
+    print(f"{v}")
+
+#quando se vai fazer só com as chaves se faz assim 
+for c, in dados.keys(): #keys, em inglês chaves ddddddddddddd
+    print(f"{c}")
